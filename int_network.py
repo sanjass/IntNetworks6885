5c7177940d0bf819554f4821e087492ede00516a
Sanja Simonovikj <sanjas@mit.edu>
2020-05-07 00:30:20 -0500

    after loss dict

--- int_network.py
+++ int_network.py
@@ -39,16 +39,32 @@ optimizer = optim.Adam(interaction_network.parameters())
 #criterion = nn.MSELoss()
 
 
+n_epoch = 200
+batches_per_epoch = 100
+loss_methods = {k: nn.MSELoss(reduction='none') for k in mse_indices}
+loss_methods.update({k: nn.CrossEntropyLoss(reduction='none') for k in binary_indices})
+loss_methods.update({k: nn.CrossEntropyLoss(reduction='none') for k in categorical_indices})
+
+
+def add_to_epoch_loss_dict(epoch_loss_dict, loss_dict):
+    for key in loss_dict:
+        if key not in epoch_loss_dict:
+            epoch_loss_dict[key] = []
+        epoch_loss_dict[key].append(loss_dict[key])
+    return epoch_loss_dict
+
 
 def validate(validation_loader, model):
     losses = []
+    epoch_val_loss_dict = dict()
     for dp, target in validation_loader:
         objects, sender_relations, receiver_relations, relation_info = dp
         predicted = interaction_network(objects, sender_relations, receiver_relations, relation_info)
-        loss = calculate_total_loss(predicted, target)
+        loss, loss_dict = calculate_total_loss(predicted, target)
+        epoch_val_loss_dict = add_to_epoch_loss_dict(epoch_val_loss_dict, loss_dict)
         #loss = criterion(predicted, target)  
         losses.append(loss.item())
-    return np.mean(losses)
+    return np.mean(losses), epoch_val_loss_dict
 
 
 def mae(predicted, target):
@@ -57,19 +73,14 @@ def mae(predicted, target):
     mae_mean = diff.mean(dim=0)
     return mae_mean
 
-# def add_vars(writer, diff, epoch):
-#     writer.add_scalar("presence bit", diff[0], epoch)
-#     writer.add_scalar("Occluder bit: ", diff[1], epoch)
-#     for key, val in index_mapping.items():
-#         writer.add_scalar(key, diff[val], epoch)
+def add_vars(writer, diff, epoch):
+    
+    for key, val in index_mapping.items():
+        if len(val)>1:
+            continue # skip categorical vars
+        writer.add_scalar(key, diff[val[0]], epoch)
 
 
-
-n_epoch = 200
-batches_per_epoch = 100
-loss_methods = {k: nn.MSELoss(reduction='none') for k in mse_indices}
-loss_methods.update({k: nn.CrossEntropyLoss(reduction='none') for k in binary_indices})
-loss_methods.update({k: nn.CrossEntropyLoss(reduction='none') for k in categorical_indices})
 
 
 def is_present_mask(is_present_target):
@@ -78,11 +89,16 @@ def is_present_mask(is_present_target):
     is_present == is_present.float()
     return is_present
 
+def average_out_dict(my_dict):
+    new_dict = dict()
+    for key, val in my_dict.items():
+        new_dict[key] = np.mean(val)
+    return new_dict
+
 
 def calculate_total_loss(predicted, target):
+    """returns batch loss and batch loss dict"""
 
-    # criterion = nn.MSELoss(reduction='mean')
-    # return criterion(predicted, target)
     loss_dict = dict()
     loss = 0
     mask = None
@@ -96,10 +112,6 @@ def calculate_total_loss(predicted, target):
     denominator = mask.sum()
     for key, val in index_mapping.items():
         cur_criterion = loss_methods[key]
-
-        # if "CrossEntropyLoss" in str(cur_criterion):
-        #     continue
-        
         cur_target = target[:,:,val[0]:val[-1]+1].reshape(-1,len(val))
         
         if "CrossEntropyLoss" in str(cur_criterion):
@@ -107,23 +119,19 @@ def calculate_total_loss(predicted, target):
             cur_target = cur_target.long()
 
         cur_predicted = predicted[:,:,val[0]:val[-1]+1].reshape(-1, len(val))
-        cur_loss = cur_criterion(cur_predicted, cur_target)
-        # print("\nkey: ", key)
-        # print("predicted: ", cur_predicted[0])
-        # print("cur target: ", cur_target[0].item())
-        # print("loss: ", cur_loss[0].item())
-        cur_loss = cur_loss.reshape(-1)
+        cur_loss = cur_criterion(cur_predicted, cur_target).reshape(-1)
+
         assert cur_loss.shape == mask.shape
-        cur_loss = (mask * cur_loss)
-     
-        cur_loss = cur_loss.sum()
+        cur_loss = (mask * cur_loss).sum()
         
         if denominator > 0:
             cur_loss /= denominator
 
         loss_dict[key] = cur_loss.item()
         loss +=  cur_loss
-    return loss
+    return loss, loss_dict
+
+
 
 
 all_train_losses = []
@@ -131,6 +139,7 @@ all_valid_losses = []
 best_val = 1000000000
 for epoch in tqdm(range(1,n_epoch+1)):
     losses = []
+    epoch_train_loss_dict = dict()
     running_mae = torch.zeros(data.shape[-1]).to(device)
     for dp, target in train_dataloader:
         objects, sender_relations, receiver_relations, relation_info = dp
@@ -139,18 +148,25 @@ for epoch in tqdm(range(1,n_epoch+1)):
         running_mae += mae_mean
         
         #loss = criterion(predicted, target)
-        loss = calculate_total_loss(predicted, target)
+        loss, loss_dict = calculate_total_loss(predicted, target)
+        epoch_train_loss_dict = add_to_epoch_loss_dict(epoch_train_loss_dict, loss_dict)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         losses.append(loss.item())
 
     running_mae /= len(train_dataloader)
-    # add_vars(writer, running_mae, epoch)
+    add_vars(writer, running_mae, epoch)
     train_loss = np.mean(losses)
-    valid_loss = validate(validation_dataloader, interaction_network)  
+    valid_loss, epoch_val_loss_dict = validate(validation_dataloader, interaction_network) 
+    epoch_train_loss_dict = average_out_dict(epoch_train_loss_dict)
+    epoch_val_loss_dict = average_out_dict(epoch_val_loss_dict)
+
     print("valid_loss: ", valid_loss)
     writer.add_scalars("Losses", {'train_loss': train_loss, 'valid_loss' : valid_loss}, epoch)
+    for key, _ in epoch_train_loss_dict.items():
+        writer.add_scalars("Per var loss: %s"%key, {'train_loss': epoch_train_loss_dict[key], \
+            'valid_loss' : epoch_val_loss_dict[key]}, epoch)
     
     all_train_losses.append(train_loss)
     all_valid_losses.append(valid_loss) 
